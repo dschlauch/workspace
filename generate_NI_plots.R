4123cf04472ab39767f80a885ae17b019dfe266c
Dan Schlauch <dschlauch@gmail.com>
2016-02-28 22:04:22 -0500

    more NI comparison scripts

--- generate_NI_plots.R
+++ generate_NI_plots.R
@@ -1,44 +1,114 @@
 library(ggplot2)
 setwd('~')
 outputDir <- "NI_only_0001"
-eclipseCases <- readRDS(file.path(outputDir,'ECLIPSE_cases_bere_network.rds'))
-eclipseControls <- readRDS(file.path(outputDir,'ECLIPSE_controls_bere_network.rds'))
-COPDGeneCases <- readRDS(file.path(outputDir,'COPDGene_cases_bere_network.rds'))
-COPDGeneControls <- readRDS(file.path(outputDir,'COPDGene_controls_bere_network.rds'))
+
+generateNIDifferencePlot <- function(datasetA, datasetB, niMethod, imageType=png){
+  datasetACases <- readRDS(file.path(outputDir, paste0(datasetA, '_cases_', niMethod, '_network.rds')))
+  datasetAControls <- readRDS(file.path(outputDir, paste0(datasetA, '_controls_', niMethod, '_network.rds')))
+  datasetBCases <- readRDS(file.path(outputDir, paste0(datasetB, '_cases_', niMethod, '_network.rds')))
+  datasetBControls <- readRDS(file.path(outputDir, paste0(datasetB, '_controls_', niMethod, '_network.rds')))
+  
+  matchedTFs <- intersect(rownames(datasetACases),rownames(datasetBCases))
+  matchedGenes <- intersect(colnames(datasetACases),colnames(datasetBCases))
+  
+  datasetACases    <- datasetACases[matchedTFs,matchedGenes]
+  datasetAControls <- datasetAControls[matchedTFs,matchedGenes]
+  datasetBCases    <- datasetBCases[matchedTFs,matchedGenes]
+  datasetBControls <- datasetBControls[matchedTFs,matchedGenes]
+  
+  
+  df <- data.frame(dataA=c(datasetACases)-c(datasetAControls), dataB=c(datasetBCases)-c(datasetBControls))
+  print(cor(df), method="spearman")
+  imageType(file.path(outputDir,paste(datasetA, datasetB, niMethod, 'edgeweight_difference_comparison.png', sep="_")))
+  plot <- ggplot(df, aes(x=dataA, y=dataB)) +
+    geom_point(size=.1, alpha=.1) + xlab(datasetA) + ylab(datasetB) + ggtitle(paste0("Edgeweights in Controls (", datasetA, datasetB, ")")) + theme_classic()
+  print(plot)
+  dev.off()
+  
+}
+
+generateNIDifferencePlot("ECLIPSE","COPDGene","bere",png)
+generateNIDifferencePlot("ECLIPSE","COPDGene","WGCNA",png)
+
+eclipseBERECases <- readRDS(file.path(outputDir,'ECLIPSE_cases_bere_network.rds'))
+eclipseBEREControls <- readRDS(file.path(outputDir,'ECLIPSE_controls_bere_network.rds'))
+COPDGeneBERECases <- readRDS(file.path(outputDir,'COPDGene_cases_bere_network.rds'))
+COPDGeneBEREControls <- readRDS(file.path(outputDir,'COPDGene_controls_bere_network.rds'))
 
 
-matchedTFs <- intersect(rownames(COPDGeneCases),rownames(eclipseCases))
-matchedGenes <- intersect(colnames(COPDGeneCases),colnames(eclipseCases))
+matchedTFs <- intersect(rownames(COPDGeneBERECases),rownames(eclipseBERECases))
+matchedGenes <- intersect(colnames(COPDGeneBERECases),colnames(eclipseBERECases))
 
-COPDGeneCases <- COPDGeneCases[matchedTFs,matchedGenes]
-COPDGeneControls <- COPDGeneControls[matchedTFs,matchedGenes]
-eclipseCases <- eclipseCases[matchedTFs,matchedGenes]
-eclipseControls <- eclipseControls[matchedTFs,matchedGenes]
+COPDGeneBERECases <- COPDGeneBERECases[matchedTFs,matchedGenes]
+COPDGeneBEREControls <- COPDGeneBEREControls[matchedTFs,matchedGenes]
+eclipseBERECases <- eclipseBERECases[matchedTFs,matchedGenes]
+eclipseBEREControls <- eclipseBEREControls[matchedTFs,matchedGenes]
 
-df <- data.frame(cases=c(COPDGeneCases),controls=c(COPDGeneControls))
+df <- data.frame(cases=c(COPDGeneBERECases),controls=c(COPDGeneBEREControls))
 cor(df)
 png(file.path(outputDir,'COPDGene_edgeweight_comparison.png'))
 ggplot(df, aes(x=controls, y=cases)) +
   geom_point(size=.1, alpha=.1) + xlab("Controls") + ylab("Cases") + ggtitle("Edgeweights in COPDGene")+ theme_classic() 
 dev.off()
 
-df <- data.frame(cases=c(eclipseCases),controls=c(eclipseControls))
+df <- data.frame(cases=c(eclipseBERECases),controls=c(eclipseBEREControls))
 cor(df)
 png(file.path(outputDir,'ECLIPSE_edgeweight_comparison.png'))
 ggplot(df, aes(x=controls, y=cases)) +
   geom_point(size=.1, alpha=.1) + xlab("Controls") + ylab("Cases") + ggtitle("Edgeweights in ECLIPSE")+ theme_classic() 
 dev.off()
 
-df <- data.frame(eclipse=c(eclipseCases),copdgene=c(COPDGeneCases))
+df <- data.frame(eclipse=c(eclipseBERECases),copdgene=c(COPDGeneBERECases))
 cor(df)
 png(file.path(outputDir,'COPDGene_vs_ECLIPSE_edgeweight_cases_comparison.png'))
 ggplot(df, aes(x=eclipse, y=copdgene)) +
   geom_point(size=.1, alpha=.1) + xlab("ECLIPSE") + ylab("COPDGene") + ggtitle("Edgeweights in Cases (ECLIPSE,COPDGene)") + theme_classic() 
 dev.off()
 
-df <- data.frame(eclipse=c(eclipseControls),copdgene=c(COPDGeneControls))
+df <- data.frame(eclipse=c(eclipseBEREControls),copdgene=c(COPDGeneBEREControls))
 cor(df)
 png(file.path(outputDir,'COPDGene_vs_ECLIPSE_edgeweight_controls_comparison.png'))
+ggplot(df, aes(x=eclipse, y=copdgene)) +
+  geom_point(size=.1, alpha=.1) + xlab("ECLIPSE") + ylab("COPDGene") + ggtitle("Edgeweights in Controls (ECLIPSE,COPDGene)") + theme_classic() 
+dev.off()
+
+# Edgeweight Difference
+
+df <- data.frame(eclipse=c(eclipseBERECases)-c(eclipseBEREControls), copdgene=c(COPDGeneBERECases)-c(COPDGeneBEREControls))
+cor(df)
+png(file.path(outputDir,'COPDGene_ECLIPSE_edgeweight_difference_comparison.png'))
+ggplot(df, aes(x=eclipse, y=copdgene)) +
+  geom_point(size=.1, alpha=.1) + xlab("ECLIPSE") + ylab("COPDGene") + ggtitle("Edgeweights in Controls (ECLIPSE,COPDGene)") + theme_classic() 
+dev.off()
+
+
+
+# Only TF-TF
+TFsWithGenes <- intersect(matchedTFs,matchedGenes)
+
+COPDGeneBERECasesTFs <- COPDGeneBERECases[TFsWithGenes,TFsWithGenes]
+COPDGeneBEREControlsTFs <- COPDGeneBEREControls[TFsWithGenes,TFsWithGenes]
+eclipseBERECasesTFs <- eclipseBERECases[TFsWithGenes,TFsWithGenes]
+eclipseBEREControlsTFs <- eclipseBEREControls[TFsWithGenes,TFsWithGenes]
+
+
+df <- data.frame(eclipse=c(COPDGeneBERECasesTFs),copdgene=c(COPDGeneBEREControlsTFs))
+cor(df)
+png(file.path(outputDir,'COPDGene_TFTF_edgeweight_controls_comparison.png'))
+ggplot(df, aes(x=eclipse, y=copdgene)) +
+  geom_point(size=.1, alpha=.1) + xlab("ECLIPSE") + ylab("COPDGene") + ggtitle("Edgeweights in Controls (ECLIPSE,COPDGene)") + theme_classic() 
+dev.off()
+
+df <- data.frame(eclipse=c(eclipseBEREControlsTFs),copdgene=c(COPDGeneBEREControlsTFs))
+cor(df)
+png(file.path(outputDir,'COPDGene_vs_ECLIPSE_TFTF_edgeweight_controls_comparison.png'))
+ggplot(df, aes(x=eclipse, y=copdgene)) +
+  geom_point(size=.1, alpha=.1) + xlab("ECLIPSE") + ylab("COPDGene") + ggtitle("Edgeweights in Controls (ECLIPSE,COPDGene)") + theme_classic() 
+dev.off()
+
+df <- data.frame(eclipse=c(eclipseBERECasesTFs),copdgene=c(COPDGeneBERECasesTFs))
+cor(df)
+png(file.path(outputDir,'COPDGene_vs_ECLIPSE_TFTF_edgeweight_controls_comparison.png'))
 ggplot(df, aes(x=eclipse, y=copdgene)) +
   geom_point(size=.1, alpha=.1) + xlab("ECLIPSE") + ylab("COPDGene") + ggtitle("Edgeweights in Controls (ECLIPSE,COPDGene)") + theme_classic() 
 dev.off()
